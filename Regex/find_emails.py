d06bc0d4b932c103c2ae977d549dcf2011ddc378
Jordan Ackerman <Verbal@Jordans-Air.hsd1.ca.comcast.net>
2017-02-17 22:34:49 -0800

    cleaning

--- Regex/find_emails.py
+++ Regex/find_emails.py
@@ -8,19 +8,21 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S
 if __name__ == '__main__':
 	logging.info('Reading File...')
 	data = sys.stdin.read()
+
+	email_pattern = r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5})'
+
+	matches = re.findall(email_pattern, data)
+	
+	# In reality one would probably want to output this to a file or variable somewhere, 
+	# but for now, just the print, since im not sure what to use this for at the moment
+	for match in matches:
+		print match
+	print len(matches), 'Matches'
+	
+	logging.info('Done.')
 else:
-	print 'This program takes a file in std in, run me from the command line - python find_emails.py < myfile.txt'
+	# if you try to import me ...
+	logging.error('This program takes a file in std in, run me from the command line - python find_emails.py < myfile.txt')
 
 
-email_pattern = r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5})'
 
-matches = re.findall(email_pattern, data)
-
-# In reality one would probably want to output this to a file or variable somewhere, 
-# but for now, just the print, since im not sure what to use this for at the moment
-for match in matches:
-	print match
-print len(matches), 'Matches'
-
-
-logging.info('Done.')
